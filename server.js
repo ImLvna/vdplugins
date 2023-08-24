2a1c1dbea0932f75b3708aaeae1327f9f77f2f92
Luna <imlvnaa@gmail.com>
2023-08-24 09:11:44 -0400

    Add a cool 404 page :D

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   console.log(`Fetching ${req.url.split("?")[0]}`);
-  express.static("dist")(req, res);
+  next();
+});
+app.use(express.static("dist", { fallthrough: true }));
+app.use(express.static("public", { fallthrough: true }));
+// Handle 404
+app.use((req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
 });
 
 app.listen(port, () => {
